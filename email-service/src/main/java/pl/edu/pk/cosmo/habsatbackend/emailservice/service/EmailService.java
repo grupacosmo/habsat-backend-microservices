a18a5819c73eaeaed4a4466379d9fdb7b5d6a8d5
Mateusz Jamróz <jamr.mat@gmail.com>
2022-11-13 10:12:20 +0100

    cloud config server and client configuration

--- email-service/src/main/java/pl/edu/pk/cosmo/habsatbackend/emailservice/service/EmailService.java
+++ email-service/src/main/java/pl/edu/pk/cosmo/habsatbackend/emailservice/service/EmailService.java
@@ -1,12 +1,12 @@
 package pl.edu.pk.cosmo.habsatbackend.emailservice.service;
 
 import pl.edu.pk.cosmo.habsatbackend.emailservice.entity.request.SendMailRequest;
-import pl.edu.pk.cosmo.habsatbackend.emailservice.entity.request.SendManyMailsRequest;
+import pl.edu.pk.cosmo.habsatbackend.emailservice.entity.request.SendMailsByRoleRequest;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 public interface EmailService {
     void sendSimpleMessage(SendMailRequest sendMailRequest) throws MalformedURLException, URISyntaxException;
-    void sendToMany(SendManyMailsRequest sendMailRequest);
+    void sendToMany(SendMailsByRoleRequest sendMailRequest);
 }
